af93e0fd808effeb1ea104b08a28b4919e3482b5
Piet Brömmel <piet.broemmel@iml.fraunhofer.de>
2024-05-09 10:53:36 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# deutsche-bahn-data
+# Deutsche Bahn Data
+
+This is a repository with accumulated public data from "Deutsche Bahn", the biggest german train company.
+
+## Data Gathering
+
+### Die größten Bahnhöfe ermitteln
+
+Die einfachste Möglichkeit die größten Bahnhöfe Deutschlands zu bekommen ist über die [Preisklasse](https://de.wikipedia.org/wiki/Preisklasse). Diese gibt indirekt an wie groß ein Bahnhof ist. Dazu habe ich eine [aktuelle Tabelle](https://www.deutschebahn.com/resource/blob/11895816/ef4ecf6dd8196c7db3ab45609d8a2034/Stationspreisliste-2024-data.pdf) aller Bahnhöfe mit ihren Preisklassen gefunden. Das Problem ist das ich noch die eva Nummer der Bahnhöfe brauche für die API.
+
+https://wiki.openstreetmap.org/w/images/c/c2/20141001_IBNR.pdf (Daten von 1.10.2014) die Zuweisung von dem Namen des ahnhofs zu ihrer IBNR-Nummer (in der api heißt wir die nummer eva gennant). Die API woher die Daten kommen gibt es nicht mehr (https://data.deutschebahn.com/dataset/data-haltestellen), daher wird eine alte Versionhier benutzt. In ihr habe ich aber die Nummern für alle relevanten Bahnhöfe gefunden.
+
+Diese beiden Datenquellen werden in `save_eva_name_list.py` benutzt um eine Liste der ~100 größten Bahnhöfe Deutschlands mit Name und eva Nummer zu erstellen.
+
+Dies sind die Befehle um die Liste selber zu erstellen:
+
+```bash
+# download the two pdfs with the data
+wget https://www.deutschebahn.com/resource/blob/11895816/ef4ecf6dd8196c7db3ab45609d8a2034/Stationspreisliste-2024-data.pdf
+wget https://wiki.openstreetmap.org/w/images/c/c2/20141001_IBNR.pdf
+
+# install dependancies for the script
+pip3 install tabula-py PyPDF2
+
+# run the script
+python3 save_eva_name_list.py
+```
+
+Wenn jemand eine aktuelle Liste von Bahnhöfen und ihrer eva nummer findet erstellt gerne ein issue.
+
---
+++ fetch_data.py
@@ -0,0 +1,59 @@
+import os
+import time
+import requests
+from datetime import datetime
+from xml.dom.minidom import parseString
+from pathlib import Path
+
+# Retrieve the secret API key from the environment variable
+api_key = os.getenv('API_KEY')
+if not api_key:
+    raise ValueError("No API Key provided!")
+
+client_id = os.getenv('CLIENT_ID')
+if not client_id:
+    raise ValueError("No Client Id provided!")
+
+headers = {
+    'DB-Api-Key': api_key,
+    'DB-Client-Id': client_id,
+    'accept': 'application/xml'
+}
+
+def save_api_data(formatted_url, save_path, prettify=True):
+    response = requests.get(formatted_url, headers=headers)
+    if response.status_code != 200:
+        print(f"error {response.status_code}: {formatted_url}")
+        return
+    with (save_path).open("w") as f:
+        if prettify:
+            f.write(parseString(response.content).toprettyxml())
+        else:
+            f.write(parseString(response.content).toxml())
+    time.sleep(1/60) # because the can be a maximum of 60 requests per minute
+
+def main():
+    plan_url = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/{eva}/{date}/{hour}"
+    fchg_url = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/fchg/{eva}"
+
+    # 
+    date_str = datetime.now().strftime("%Y-%m-%d")
+    date_str_url = date_str.replace("-", "")[2:]
+
+    save_folder = Path("data") / date_str
+    save_folder.mkdir(exist_ok=True)
+
+    with Path("eva_name_list.txt").open("r") as f:
+        eva_name_list = [line.split(",") for line in f.read().split("\n")]
+
+    curent_hour = datetime.now().hour
+    for eva, name in eva_name_list:
+        formatted_fchg_url = fchg_url.format(eva=eva)
+        save_api_data(formatted_fchg_url, save_folder / f"{eva}_fchg_{curent_hour:02}.xml", prettify=False)
+        for hour in range(curent_hour, curent_hour + 6):
+            hour = hour % 24
+            formatted_plan_url = plan_url.format(eva=eva, date=date_str_url, hour=f"{hour:02}")
+            save_api_data(formatted_plan_url, save_folder / f"{eva}_plan_{hour:02}.xml")
+
+if __name__ == "__main__":
+    main()
