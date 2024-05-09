0c9b3f6a5ad43c346fb0ea97ac2fe5690bc2a38b
Piet Brömmel <piet.broemmel@iml.fraunhofer.de>
2024-05-09 10:53:36 +0200

    init

---
+++ save_eva_name_list.py
@@ -0,0 +1,45 @@
+import tabula
+import PyPDF2
+
+# get biggest station using the "Preisklasse"
+tables = tabula.read_pdf("Stationspreisliste-2024-data.pdf", pages='all')
+biggest_stations = []
+for df in tables:
+    for _, row in df[df["Preis-\rklasse"] <= 2].iterrows():
+        biggest_stations.append(row["Bahnhof"].replace("-", " ").replace(" Hauptbahnhof", " Hbf"))
+
+# read IBNR/eva PDFs as text
+with open('20141001_IBNR.pdf', 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    num_pages = len(reader.pages)
+    all_text = ""
+
+    for i in range(num_pages):
+        page = reader.pages[i]
+        all_text += page.extract_text() + "\n"
+
+# create name and eva list and do some sting manupulations to match the different names in the PDFs
+eva_name_list = []
+for eva_name in all_text.split("\n")[2:-1]:
+    eva_name_split = eva_name.split(" ")
+    name = " ".join(eva_name_split[:-1])
+    # TODO: do better fuzzy matching of the names
+    if name == "Ludwigshafen(Rh)Hbf":
+        name = "Ludwigshafen (Rhein) Hbf"
+    elif name == "Berlin Brandenburg Flughafen":
+        name = "Flughafen BER"
+    else:
+        name = name.replace("-", " ").replace("(", " (").replace(")", ") ").replace(")  ", ") ").rstrip()
+    
+    eva = eva_name_split[-1]
+    if name in biggest_stations:
+        eva_name_list.append((f"{eva},{name}"))
+    elif name.replace(" Hbf", "") in biggest_stations:
+        eva_name_list.append((f"{eva},{name.replace(' Hbf', '')}"))
+    elif f"{name} Hbf" in biggest_stations:
+        eva_name_list.append((f"{eva},{name} Hbf"))
+    
+    # TODO: there is a but that there are two "Solingen Hbf, only the lower number is right"
+
+with open("eva_name_list.txt", "w") as f:
+    f.write("\n".join(eva_name_list))
